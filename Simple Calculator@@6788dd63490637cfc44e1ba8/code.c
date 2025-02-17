0b31422a60539b3d06524729a756b6d3c1024c75
Vinayak Sharma <vinnayak.05@gmail.com>
2025-02-18 03:01:53 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,11 @@
+// Your code here...
+#include<stdio.h>
+int main(){
+    int a,b;
+    char c;
+    scanf("%d %d %c",&a,&b,&c);
+    calculator=acb;
+    printf("%d %c %d",calculator);
+
+    return 0;
+}
